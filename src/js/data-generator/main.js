fd09b814424ae22e5ab866d4bc7511092b52e121
Christian Stussak <christian.stussak@imaginary.org>
2025-09-18 13:41:54 +0200

    Improve error messages of assertions

--- src/js/data-generator/main.js
+++ src/js/data-generator/main.js
@@ -48,7 +48,7 @@ async function warmingStripeData(url) {
   annualData.forEach((v, i) =>
     assert(
       v.year === annualData[0].year + i,
-      `The temperature record contains duplicate or missing data for the reference month (June) in year ${v.year}.`,
+      `The temperature record contains duplicate or missing data for the reference month (June) in year ${v.year} (${url}).`,
     ),
   );
 
@@ -56,14 +56,14 @@ async function warmingStripeData(url) {
     assert(
       (typeof v.anomaly === 'number' && Number.isFinite(v.anomaly)) ||
         v.anomaly === null,
-      `Temperature anomaly for ${v.year} must be a finite number or null, but is ${v.anomaly}.`,
+      `Temperature anomaly (${v.anomaly}) for ${v.year} must be a finite number or null, but is ${v.anomaly} (${url}).`,
     ),
   );
 
   annualData.forEach((v) =>
     assert(
       (typeof v.unc === 'number' && Number.isFinite(v.unc)) || v.unc === null,
-      `Temperature anomaly uncertainty for ${v.year} must be a finite number or null, but is ${v.unc}.`,
+      `Temperature anomaly uncertainty (${v.unc}) for ${v.year} must be a finite number or null, but is ${v.unc} (${url}).`,
     ),
   );
 
@@ -87,7 +87,7 @@ async function warmingStripeData(url) {
   const validYearRange = [minValidYear, maxValidYear];
   assert(
     minValidYear <= maxValidYear,
-    `At least the data for the most recent year must be valid. Valid year range: ${validYearRange}.`,
+    `At least the data for the most recent year must be valid. Valid year range: ${validYearRange}  (${url}).`,
   );
 
   const { mean: mean1971To2000 } = analyzeAnomalies(annualData, 1971, 2000);
@@ -99,7 +99,7 @@ async function warmingStripeData(url) {
 
   assert(
     stdDev1901To2000 > 0.0,
-    `Standard deviation must be > 0, but is ${stdDev1901To2000}.`,
+    `Standard deviation must be > 0, but is ${stdDev1901To2000} (${url}).`,
   );
 
   const maxDev = max(
